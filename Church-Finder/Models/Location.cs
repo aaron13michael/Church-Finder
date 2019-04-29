93446220666e6ecc636d38f141cd520b88da91c1
Aaron Stadler <Aaron.Stadler@LighthouseSystems.com>
2019-04-29 16:51:41 -0400

    Add controller and model

---
+++ Church-Finder/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Church_Finder.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Religion { get; set; }
+        public string Sect { get; set; }
+        public int Members { get; set; }
+    }
+}
